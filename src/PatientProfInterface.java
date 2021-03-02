7ba951f093049551388137dead63bdc4e7252c2c
Eli <EliMarkShattuck@gmail.com>
2021-03-02 15:08:18 -0500

    Added   - basic outline of PatientProfInterface

--- src/PatientProfInterface.java
+++ src/PatientProfInterface.java
@@ -1,2 +1,45 @@
 public class PatientProfInterface {
+    public PatientProfInterface(String fileName) {
+
+    }
+
+    public void getUserChoice() {
+
+    }
+
+    public void deletePatientProf() {
+
+    }
+
+    public void findPatientProf() {
+
+    }
+
+    public void updatePatientProf() {
+
+    }
+
+    public void displayPatientProf() {
+
+    }
+
+    public void displayAllPatientProf() {
+
+    }
+
+    public void writeToDB() {
+
+    }
+
+    public void initDB() {
+
+    }
+
+    public PatientProf createNewPatientProf() {
+        return null;
+    }
+
+    public MedCond createNewMedCond() {
+        return null;
+    }
 }
