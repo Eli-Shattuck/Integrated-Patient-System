ce2fb70b99b3e687440a831d3f1b6091d8e01d82
Eli <EliMarkShattuck@gmail.com>
2021-03-02 15:02:46 -0500

    Added   - basic outline of PatientProfDB

--- src/PatientProfDB.java
+++ src/PatientProfDB.java
@@ -1,2 +1,38 @@
 public class PatientProfDB {
+    private int numPatient;
+    private int currentPatientIndex;
+    private String fileName;
+    private PatientProf[] patientList;
+
+    public PatientProfDB(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void insertNewProfile(PatientProf patientProf) {
+
+    }
+
+    public boolean deleteProfile(String adminID, String lastName) {
+        return false;
+    }
+
+    public PatientProf findProfile(String adminID, String lastName) {
+        return null;
+    }
+
+    public PatientProf findFirstProfile() {
+        return null;
+    }
+
+    public PatientProf findNextProfile() {
+        return null;
+    }
+
+    public void writeAllPatientProf() {
+
+    }
+
+    public void initializeDatabase() {
+
+    }
 }
