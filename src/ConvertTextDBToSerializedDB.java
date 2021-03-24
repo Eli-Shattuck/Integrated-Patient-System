16717fa9bc21ee6a618cf76d81c9c42f0c93052e
Eli-Shattuck <73374807+Eli-Shattuck@users.noreply.github.com>
2021-03-23 21:45:50 -0400

    Added new class to read plaintext database files nad convert them to Serialized database files

--- src/ConvertTextDBToSerializedDB.java
+++ src/ConvertTextDBToSerializedDB.java
@@ -2,21 +2,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+ * class used to convert a plaintext database file
+ * into the standard serialized list format that
+ * is used by the PatientProfDB class
+ *
+ * Each line of the plaintext file is the next attribute of a PatientProf object.
+ */
 public class ConvertTextDBToSerializedDB {
     public static void main(String[] args) throws IOException {
-        File dbFile = new File("database.txt");
-        Scanner input = new Scanner(dbFile);
+        File dbFile = new File("database.txt"); //read in the plaintext file
+        Scanner input = new Scanner(dbFile); //create a scanner from the file
 
-        PatientProfDB db = new PatientProfDB("databaseSerialize.txt");
+        PatientProfDB db = new PatientProfDB("databaseSerialize.txt"); //create a PatientProfileDB which can write files of the correct type
 
-        while(input.hasNext()) {
-            PatientProf prof = new PatientProf(
+        while(input.hasNext()) { //as long as there are more lines in the file
+            PatientProf prof = new PatientProf( //generate a new PatientProf from the data in the file
                     input.nextLine(),
                     input.nextLine(),
                     input.nextLine(),
                     input.nextLine(),
                     input.nextLine(),
-                    Float.parseFloat(input.nextLine()),
+                    Float.parseFloat(input.nextLine()), //the 6th attribute is the float coPAy, so this attribute must be parsed as a float not as a string
                     input.nextLine(),
                     input.nextLine(),
                     new MedCond(
@@ -26,9 +33,9 @@ public class ConvertTextDBToSerializedDB {
                             input.nextLine()
                     )
             );
-            db.insertNewProfile(prof);
+            db.insertNewProfile(prof); //add the profile to the database
         }
 
-        db.writeAllPatientProf();
+        db.writeAllPatientProf(); //write the database to a file
     }
 }
--- src/TestDB.java
+++ src/TestDB.java
@@ -13,64 +13,84 @@ import java.io.IOException;
  */
 public class TestDB {
     public static void main(String[] args) {
+        // If the file testing.txt exists, delete it
         File testFile = new File("testing.txt");
         testFile.delete();
 
+        // call the generate method of GenerateTestData to create a testfile
         try {
             GenerateTestData.generate("testing.txt");
         } catch (IOException e) {
             System.err.println("Error Generating Testing Data:");
             e.printStackTrace();
+            return;
         }
 
+        // create a database that reads from that file
         PatientProfDB db = new PatientProfDB("testing.txt");
         try {
             db.initializeDatabase();
         } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error Initializing Database");
             e.printStackTrace();
+            return;
         }
 
+        // use assert to make sure that profiles that are supposed to be in the database can be found
         PatientProf prof = db.findProfile(GenerateTestData.ADMIN_IDS[0], GenerateTestData.LAST_NAMES[0]);
         assert prof != null : "Error Finding Profile";
 
+        // use assert to make sure that the first profile is returned by the find first method
         prof = db.findFirstProfile();
         assert prof.getLastName().equals(GenerateTestData.LAST_NAMES[0]): "Find Next Profile Not Returning First Profile";
 
+        // use assert to make sure that the first profile is returned when find next is called for the first time
         prof = db.findNextProfile();
         assert prof.getLastName().equals(GenerateTestData.LAST_NAMES[0]): "Find Next Profile Not Returning First Profile";
 
+        // use assert to make sure that the second profile is returned when find next is called again
         prof = db.findNextProfile();
         assert prof.getLastName().equals(GenerateTestData.LAST_NAMES[1]): "Find Next Profile Not Returning Next Profile";
 
-        prof = db.findNextProfile();
-        assert prof.getLastName().equals(GenerateTestData.LAST_NAMES[2]): "Find Next Profile Not Returning First Profile";
-
+        // use assert to make sure that the first profile is returned after the patient index is returned
         db.resetCurrentPatientIndex();
         prof = db.findNextProfile();
         assert prof.getLastName().equals(GenerateTestData.LAST_NAMES[0]): "Resting DatabaseIndex Failed";
 
+        // use assert to make sure that profiles can be added to the database
         db.insertNewProfile(new PatientProf("A0", "Jane", "Doe", null, null, 0.0f, null, null, null));
         prof = db.findProfile("A0", "Doe");
         assert prof != null && prof.getFirstName().equals("Jane"): "Error Inserting New Profile";
 
+        // use assert to make sure that profiles can be deleted from the database
+        db.deleteProfile(GenerateTestData.ADMIN_IDS[2], GenerateTestData.LAST_NAMES[2]);
+        prof = db.findProfile(GenerateTestData.ADMIN_IDS[2], GenerateTestData.LAST_NAMES[2]);
+        assert prof == null : "Error Removing Profile";
+
+        // use try catch to make sure that the database can be written to
         try {
             db.writeAllPatientProf();
         } catch (IOException e) {
             System.err.println("Error Saving Database");
             e.printStackTrace();
+            return;
         }
 
+        // create a new database object from the recently saved file
         PatientProfDB db2 = new PatientProfDB("testing.txt");
         try {
             db2.initializeDatabase();
         } catch (IOException | ClassNotFoundException e) {
             System.err.println("Error Initializing Database");
             e.printStackTrace();
+            return;
         }
+
+        // use assert to make sure that profiles that are added remain after a save
         prof = db2.findProfile("A0", "Doe");
         assert prof != null && prof.getFirstName().equals("Jane"): "Error Loading Information From Database After Saving";
 
+        //if all asserts pass then report success to user
         System.out.println("All test cases passed.");
     }
 }
